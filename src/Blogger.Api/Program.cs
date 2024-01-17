07f5714f71981e4a56bebfd96025e8d583682f73
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2024-01-17 13:11:23 +0700

    add dbContext

--- src/Blogger.Api/Program.cs
+++ src/Blogger.Api/Program.cs
@@ -1,6 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using Blogger.Core.Domain.Identity;
+using Blogger.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
+
+//Config DB Context and ASP.NET Core Identity
+builder.Services.AddDbContext<BloggerContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<BloggerContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    //Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    //Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    //User setting.
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
