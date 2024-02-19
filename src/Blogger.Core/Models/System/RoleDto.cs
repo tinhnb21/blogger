a3cd0f1b59bd4aa62cafffe7dd68c1fb43abf4ec
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2024-02-19 11:24:53 +0700

    permission p1

---
+++ src/Blogger.Core/Models/System/RoleDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Blogger.Core.Domain.Identity;
+
+namespace Blogger.Core.Models.System
+{
+    public class RoleDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public class AutoMapperProfiles : Profile
+        {
+            public AutoMapperProfiles()
+            {
+                CreateMap<AppRole, RoleDto>();
+            }
+        }
+    }
+}
