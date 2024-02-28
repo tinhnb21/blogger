69307eb38032fb79735b0efc71a021dbc13d85a3
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2024-02-28 14:56:29 +0700

    List by series

---
+++ src/Blogger.WebApp/Controllers/SeriesController.cs
@@ -0,0 +1,33 @@
+﻿using Blogger.Core.SeedWorks;
+using Blogger.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogger.WebApp.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SeriesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [Route("/series")]
+        public async Task<IActionResult> Index([FromQuery] int page = 1)
+        {
+            var series = await _unitOfWork.Series.GetAllPaging(string.Empty, page);
+            return View(series);
+        }
+
+        [Route("series/{slug}")]
+        public async Task<IActionResult> Details([FromRoute] string slug)
+        {
+            var posts = await _unitOfWork.Series.GetAllPostsInSeries(slug);
+            var series = await _unitOfWork.Series.GetBySlug(slug);
+            return View(new SeriesDetailViewModel()
+            {
+                Posts = posts,
+                Series = series
+            });
+        }
+    }
+}
---
+++ src/Blogger.WebApp/Models/SeriesDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Blogger.Core.Models.Content;
+using Blogger.Core.Models;
+
+namespace Blogger.WebApp.Models
+{
+    public class SeriesDetailViewModel
+    {
+        public SeriesDto Series { get; set; }
+
+        public PagedResult<PostInListDto> Posts { get; set; }
+    }
+}
